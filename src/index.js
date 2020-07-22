c9d28effa1723836a68bc4d870bb22d57ef9ae85
jdwasham <Jeremy.19@dartmouth.edu>
2020-07-21 23:21:59 -0400

    final

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('starting up!');
+// change require to es6 import style
+import $ from 'jquery';
+import './style.scss';
+
+let timer = 0;
+
+setInterval(() => {
+  timer += 1;
+  const foo = `You've been on this page for ${timer} seconds`;
+  $('#main').html(foo);
+}, 1000);
