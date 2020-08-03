ba759577f2e673d0d4c6bacf3bf2e1c72e38fa76
jeremy-washam <Jeremy.D.Washam.19@dartmouth.edu>
2020-08-03 11:53:16 -0400

    starter v4

--- src/index.js
+++ src/index.js
@@ -1,34 +1,32 @@
 // change require to es6 import style
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import './style.scss';
 import {
   BrowserRouter as Router, Route, NavLink, Switch,
 } from 'react-router-dom';
-import './style.scss';
+import reducers from './reducers';
+import Counter from './components/counter';
+import Controls from './components/controls';
+
+// this creates the store with the reducers, and does some other stuff to initialize devtools
+// boilerplate to copy, don't have to know
+const store = createStore(reducers, {}, compose(
+  applyMiddleware(),
+  window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f,
+));
 
 const Nav = (props) => {
   return (
     <nav>
       <ul>
-        <li><NavLink to="/" exact>Home</NavLink></li>
-        <li><NavLink to="/about">About</NavLink></li>
-        <li><NavLink to="/test/id1">test id1</NavLink></li>
-        <li><NavLink to="/test/id2">test id2</NavLink></li>
+        <li><NavLink to="/" exact>Controls</NavLink></li>
+        <li><NavLink to="/counter">Counter</NavLink></li>
       </ul>
     </nav>
   );
-};
-
-const Welcome = (props) => {
-  return <div>Welcome</div>;
-};
-
-const About = (props) => {
-  return <div> All there is to know about me </div>;
-};
-
-const Test = (props) => {
-  return <div> ID: {props.match.params.id} </div>;
 };
 
 const FallBack = (props) => {
@@ -41,9 +39,8 @@ const App = (props) => {
       <div>
         <Nav />
         <Switch>
-          <Route exact path="/" component={Welcome} />
-          <Route path="/about" component={About} />
-          <Route exact path="/test/:id" component={Test} />
+          <Route exact path="/" component={Controls} />
+          <Route path="/counter" component={Counter} />
           <Route component={FallBack} />
         </Switch>
       </div>
@@ -51,4 +48,10 @@ const App = (props) => {
   );
 };
 
-ReactDOM.render(<App />, document.getElementById('main'));
+// we now wrap App in a Provider
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('main'),
+);
